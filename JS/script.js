ffd990d6c55e420864ebe1baee464d9573d29193
Raghav Sachdev <iraghavsachdev@gmail.com>
2024-08-27 10:53:49 +0530

    Changes Applied

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ function handleClick() {
     const cityName = document.getElementById('city-input').value;
     if (cityName) {
         sessionStorage.setItem('cityName', cityName);
-        window.location.href = 'loading.html';
+        window.location.href = './Pages/loading.html';
     } else {
         alert('Please enter a city name.');
     }
